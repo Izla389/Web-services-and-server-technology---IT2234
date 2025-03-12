fed62c18ebd03da6193964cf0899e7b5c58a6343
Izla389 <izlaizzath@gmail.com>
2025-03-12 10:47:18 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,18 @@
+console.log("Hello world")
+
+let n1 = 12
+let n2 = 2
+
+console.log(n1+n2)
+console.log(n1-n2)
+console.log(n1*n2)
+console.log(n1/n2)
+
+var x = 30
+var x = 10
+var y = 52
+
+console.log(x+y)
+
+const z =1.5
+console.log(z)

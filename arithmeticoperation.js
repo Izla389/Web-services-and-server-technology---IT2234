fd60795a2167c36dbb9dd18820779c907e369475
Izla389 <izlaizzath@gmail.com>
2025-03-12 10:56:27 +0530

    Add files via upload

---
+++ arithmeticoperation.js
@@ -0,0 +1,17 @@
+
+let n1 = 12
+let n2 = 2
+
+console.log(n1+n2)
+console.log(n1-n2)
+console.log(n1*n2)
+console.log(n1/n2)
+
+var x = 30
+var x = 10
+var y = 52
+
+console.log(x+y)
+
+const z =1.5
+console.log(z)
